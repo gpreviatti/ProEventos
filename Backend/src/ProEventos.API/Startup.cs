bfd6fcf83fd8bce336291dc194602c72f9988ebb
Giovanni B. Previatti <gpreviatt@gmail.com>
2021-10-19 08:21:45 -0300

    imp: backend dockerfile and swagger deffinitions

--- Backend/src/ProEventos.API/Startup.cs
+++ Backend/src/ProEventos.API/Startup.cs
@@ -1,17 +1,11 @@
 using System;
-using System.IO;
-using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using ProEventos.Application;
-using ProEventos.Application.Helpers;
 using ProEventos.Domain;
 using ProEventos.Persistence;
 
@@ -34,12 +28,15 @@ namespace ProEventos.API
                 .AddControllers()
                 .AddNewtonsoftJson(
                     j => j.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
+                );
 
             services.AddCors();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "ProEventos API", 
+                    Version = "v1"
+                });
             });
 
             // Services
@@ -60,22 +57,21 @@ namespace ProEventos.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProEventos.API v1");
-                });
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                c.InjectStylesheet("/swagger-ui/SwaggerDark.css");
+
+            });
 
             // Configurando o storage das imagens
-            // app.UseStaticFiles(new StaticFileOptions() { 
-            //     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Resources" )),
-            //     RequestPath = new PathString("/Resources")
-            // });
+            app.UseStaticFiles(new StaticFileOptions() { 
+                // FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Resources" )),
+                // RequestPath = new PathString("/Resources")
+            });
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
