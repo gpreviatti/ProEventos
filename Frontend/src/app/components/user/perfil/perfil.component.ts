decce8dddfddb75bec33df963a87bfef5cbfc20c
Giovanni B. Previatti <gpreviatt@gmail.com>
2022-02-19 13:37:58 -0300

    feat: ajustes minoritarios - Perfil component - Nav  component

--- Frontend/src/app/components/user/perfil/perfil.component.ts
+++ Frontend/src/app/components/user/perfil/perfil.component.ts
@@ -18,12 +18,18 @@ export class PerfilComponent implements OnInit {
 
   public user = {} as UserUpdate;
 
+  formOptions: AbstractControlOptions = {
+    validators: this.formHelper.mustMatch('password', 'confirmPassword')
+  };
+
   form = this.formBuilder.group({
     userName: ['', [Validators.required, Validators.minLength(4), Validators.max(120000)]],
     firstName: ['', [Validators.required, Validators.minLength(4), Validators.max(120000)]],
     lastName: ['', [Validators.required, Validators.minLength(4), Validators.max(120000)]],
-    email: ['', [Validators.required, Validators.email]]
-  });
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required ]],
+    confirmPassword: ['', [Validators.required ]]
+  }, this.formOptions);
 
   formControls = this.form.controls;
 
@@ -56,8 +62,7 @@ export class PerfilComponent implements OnInit {
         this.accountService.setUser(user);
         this.toaster.success('Perfil atualizado com sucesso!');
       },
-      (error: any) =>  this.toaster.error(error.message),
-      () => this.spinner.hide()
-    );
+      (error: any) =>  this.toaster.error(error.message)
+    ).add(() => this.spinner.hide());
   }
 }
